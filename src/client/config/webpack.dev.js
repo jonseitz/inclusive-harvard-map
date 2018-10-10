f4e414375873b954ca8f7a7ed28be1ee77530600
Jonathan Seitz <seitz.jon@gmail.com>
2018-10-09 22:01:42 -0400

    starting webpack config

---
+++ src/client/config/webpack.dev.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  name: 'client',
+  devtool: 'cheap-module-source-map',
+  mode: 'development',
+  target: 'web',
+  context: path.resolve(__dirname, '..'),
+  entry: path.resolve(__dirname, '../index.js'),
+  output: {
+    path: path.resolve(__dirname, '../build/'),
+    filename: '[name].bundle.js',
+  },
+  watch: true,
+  watchOptions: {
+    poll: true,
+    aggregateTimeout: 300,
+    ignored: [/node_modules/, /__tests__/],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      // {
+      //   test: /\.js$/,
+      //   include: [path.resolve(__dirname, '../')],
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['react'],
+      //       // plugins: [
+      //       //   'transform-object-rest-spread',
+      //       //   'transform-decorators-legacy',
+      //       //   'transform-class-properties',
+      //       // ],
+      //     },
+      //   },
+      // },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Inclusive Harvard Mapping',
+      inject: true,
+    }),
+  ],
+};
