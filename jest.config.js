82cef33a6ff730de63bd7cd4ece1e28158791bc8
Jonathan Seitz <seitz.jon@gmail.com>
2018-10-16 16:55:00 -0400

    Adding Jest configurations
    Uses the projects feature in Jest to separate client and server code and
    run both from the single `jest` cli tool

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    'src/server/config/jest.config.js',
+    'src/client/config/jest.config.js',
+  ],
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+};
