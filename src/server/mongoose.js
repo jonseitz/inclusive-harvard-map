190ae5c5435b14b97de028ab0505720dcaf3170f
Jonathan Seitz <seitz.jon@gmail.com>
2018-10-21 23:31:31 -0400

    move mongoose config into a single file

---
+++ src/server/mongoose.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import models from './models';
+
+const db = mongoose.createConnection(
+  `mongodb://${process.env.MONGO_URI}/${process.env.MONGO_INITDB_DATABASE}`,
+  {
+    keepAlive: true,
+    useNewUrlParser: true,
+    user: process.env.MONGO_USER,
+    pass: process.env.MONGO_PASS,
+  }
+);
+
+db.model('Building', models.BuildingSchema);
+db.model('Facility', models.FacilitySchema);
+db.model('Floor', models.FloorSchema);
+
+export default db;
