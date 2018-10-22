b2f89d89b2a87fc620f2c0d313d94dc61c39df13
Jonathan Seitz <seitz.jon@gmail.com>
2018-10-21 23:32:07 -0400

    setting up logging and error handling

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,44 @@
 import express from 'express';
+import https from 'https';
+import http from 'http';
+import morgan from 'morgan';
+import floorplanRouter from './routes/floorplans';
+import buildingRouter from './routes/buildings';
 
 const app = express();
 
-app.listen(process.env.SERVER_PORT);
+app.use(morgan('tiny'));
 
-app.on('SIGHUP', () => {
+app.use('/api', express.json({ type: '*/*' }));
+
+app.use('/api', (req, res, next) => {
+  res.set('Content-Type', 'application/json');
+  next();
+});
+
+app.use('/api/floorplans', floorplanRouter);
+app.use('/api/buildings', buildingRouter);
+
+export const errorHandler = (err, req, res) => {
+  res.status(500);
+  res.json({ error: err.message });
+  res.end();
+};
+
+app.use(errorHandler);
+
+http.createServer(app).listen(process.env.SERVER_PORT, (err) => {
+  if (!err) {
+    console.log(`listening on ${process.env.SERVER_PORT}`);
+  }
+});
+
+if (process.env.SERVER_PORT_SSL) {
+  https.createServer(app).listen(process.env.SERVER_PORT_SSL);
+}
+
+app.on('SIGINT', () => {
   process.exit(0);
 });
+
+export default app;
