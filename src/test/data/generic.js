6862c1606ce2097254ac46a967d50620d8183387
Jonathan Seitz <seitz.jon@gmail.com>
2018-11-08 23:26:38 -0500

    creating fake MongoID

--- src/test/data/generic.js
+++ src/test/data/generic.js
@@ -1,3 +1,5 @@
+import { Types } from 'mongoose';
+
 export const string = "Don't Panic";
 
 export const safeString = 'dont-panic';
@@ -7,3 +9,5 @@ export const int = 42;
 export const array = ['life', 'universe', 'everything'];
 
 export const error = new Error('Error');
+
+export const mongoId = Types.ObjectId('07f191e810c19729de860eaf');
