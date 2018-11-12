6a0fc168f1e6dde788b61d2b307c0e14532aa3bc
Jonathan Seitz <seitz.jon@gmail.com>
2018-11-11 22:54:11 -0500

    deduplicating jest configs

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 module.exports = {
   projects: [
     'src/server/config/jest.config.js',
     'src/client/config/jest.config.js',
   ],
-  verbose: true,
+  verbose: false,
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  timers: 'fake',
 };
