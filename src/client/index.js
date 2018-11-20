3eee43250a22894dfd67e3d1c8fff9be82f5a266
Jonathan Seitz <seitz.jon@gmail.com>
2018-11-19 23:49:04 -0500

    import roboto font

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/components/App';
+import 'typeface-roboto';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 if (module.hot) {
   module.hot.accept('./js/components/App', () => {
