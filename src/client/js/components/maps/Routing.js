f5b5e769bc51390992f1c34484e3370b02140c54
Jonathan Seitz <seitz.jon@gmail.com>
2018-12-05 00:11:34 -0500

    adding routing component

---
+++ src/client/js/components/maps/Routing.js
@@ -0,0 +1,40 @@
+import { MapLayer, withLeaflet } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet-routing-machine';
+
+class Routing extends MapLayer {
+  createLeafletElement({ from, to }) {
+    return L.Routing.control({
+      waypoints: [
+        L.latLng(...from),
+        L.latLng(...to),
+      ],
+      serviceUrl: '/osrm',
+      profile: 'foot',
+      show: false,
+    });
+  }
+
+  updateLeafletElement(oldProps, newProps) {
+    if (
+      oldProps.to[0] !== newProps.to[0] || oldProps.to[1] !== newProps.to[1]
+    ) {
+      const { to } = newProps;
+      this.leafletElement.spliceWaypoints(1, 1, L.latLng(...to));
+    }
+    if (
+      oldProps.from[0] !== newProps.from[0]
+      || oldProps.from[1] !== newProps.from[1]
+    ) {
+      const { from } = newProps;
+      this.leafletElement.spliceWaypoints(0, 1, L.LatLng(...from));
+    }
+  }
+
+  componentDidMount() {
+    const { map } = this.props.leaflet;
+    this.leafletElement.addTo(map);
+  }
+}
+
+export default withLeaflet(Routing);
